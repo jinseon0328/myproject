1b1967754c184ee59c69640c8faf6a50437c3343
jinseon0328 <wishofsun@daum.net>
2021-04-14 00:45:21 +0900

    2021-04-13

--- app/src/main/java/com/jinseon0328/myproject/App.java
+++ app/src/main/java/com/jinseon0328/myproject/App.java
@@ -51,8 +51,7 @@ public class App {
   // -> 적용할 예정임
   // 24-d 적용하기 위해 모든 메뉴를 문자로 바꾸기로 했음
   // 25 적용하고 있는중!
-  //
-  //
+  // 2021-04-18 목표!
 
   static LinkedList<Board> dramaboardList = new LinkedList<>();
   static ArrayList<Movie> aftermovieList = new ArrayList<>();
